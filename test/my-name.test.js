f19fefbdf706bd09284067d0a6e7f9693d10ca1c
Mohit Sharma <developer.pht@gmail.com>
2015-12-01 18:39:12 +0530

    Testing With Mocha and Chai

---
+++ test/my-name.test.js
@@ -0,0 +1,14 @@
+var expect = require('chai').expect;
+describe('Name database model', function() {
+   var nameDB = require('../lib/name-db');
+    describe('getRandomNames()', function() {
+       describe('when give an argument of 3', function() {
+           it('should return 3 random names', function() {
+               var names = nameDB.getRandomNames(3);
+               console.log(names);
+               expect(names).to.be.an('array').with.length(3);
+               expect(names[0]).to.be.a("string");
+           });
+       });
+    });
+});
